44927c28b1ceef49a72b9c9934c3fb0fb8aefbcb
M7mD.A.MaHDi <96599149+Moh-A-Mahdi@users.noreply.github.com>
2022-10-14 18:57:38 +0200

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+
 /**
   * Description: main - Prints a hexadecimal string
   * Return: Always (Success)
   */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-putchar(c);
-}
-putchar('\n');
-return (0); 
+  char c;
+  for (c = '0'; c <= '9'; c++)
+  {
+    putchar(c);
+  }
+
+  for (c = 'a'; c <= 'f'; c++)
+  {
+    putchar(c);
+  }
+  putchar('\n');
+  return (0); 
 }
