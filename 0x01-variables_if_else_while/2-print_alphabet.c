c13cc9806e58d8e2b8ec717d876250c714d99a9f
Moh A. Mahdi <moh.mahdi0548@gmail.com>
2022-10-14 08:12:29 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
-
  * Description: main - Prints the alphabet in lowercase.
-
  * Return: Always 0.
-
  */
 
 int main(void)
-
 {
-
-char letter;
-
-
-
-for (letter = a; letter <= z; letter++)
-
-putchar(letter);
-
-
-
-putchar(n);
-
-
-
-return (0);
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter)
+	putchar(n);
+	return (0);
 
 }
