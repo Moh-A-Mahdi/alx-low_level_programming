10fffa99ed3951bccc1d32e49b84732d260c23f6
M7mD.A.MaHDi <96599149+Moh-A-Mahdi@users.noreply.github.com>
2022-10-26 17:55:00 +0200

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,9 +3,7 @@
 /**
  * _strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
- * @dest: the destiation string to be concatenated upon
- * 
- * Return:pointer to the resulting string
+ * @dest: the destiation string
  */
 
 char *_strcat(char *dest, char *src)
