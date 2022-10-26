e4d3836d040f47661e526aa546bc928a77400cf6
M7mD.A.MaHDi <96599149+Moh-A-Mahdi@users.noreply.github.com>
2022-10-26 17:53:35 +0200

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strcat - concatenates @src to @dest
+ * _strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
  * @dest: the destiation string to be concatenated upon
  * Return:pointer to the resulting string
@@ -18,7 +18,9 @@ char *_strcat(char *dest, char *src)
 	while (src[i] != '\0')
 	{
 		dest[dest_len] = src[i];   
+	
 		dest_len++;
+	
 		i++;
 	}
     
