e74dfc83981fadb6ed8553202220cdaac512812e
M7mD.A.MaHDi <96599149+Moh-A-Mahdi@users.noreply.github.com>
2022-10-26 18:47:44 +0200

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *leet(char *s)
+{
+	int index;
+	int tmp;
+	int tmp_2;
+
+	char leet[] = "oOlLeEaAtT";
+	char rep[] = "0011334477";
+
+	index = 0;
+	while (s[index] != '\0')
+	{
+		tmp = 0;
+		tmp_2 = 0;
+		while (leet[tmp] != '\0')
+		{
+			if (s[index] == leet[tmp])
+			{
+				tmp_2 = tmp;
+				s[index] = rep[tmp_2];
+			}
+			tmp++;
+		}
+		index++;
+	}
+	return (s);
+}
