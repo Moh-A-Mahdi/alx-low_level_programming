28f58f753c8eacc876ca7e7de65aebdae7d74373
M7mD.A.MaHDi <96599149+Moh-A-Mahdi@users.noreply.github.com>
2022-10-26 17:56:10 +0200

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,28 +3,27 @@
 /**
  * _strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
- * @dest: the destiation string
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
+
 	int i = 0;
 
 	while (dest[dest_len] != '\0')
-        dest_len++;
+		dest_len++;
 
 	while (src[i] != '\0')
 	{
-		dest[dest_len] = src[i];   
-	
+		dest[dest_len] = src[i];
 		dest_len++;
-	
 		i++;
 	}
-    
+
 	dest[dest_len] = '\0';
 
 	return (dest);
 }
-
